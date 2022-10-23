b2184d809efef585de29b7ae18009f5dcc9b1acb
Ibrahim Topchu <mr.ibrahim.topchu@outlook.com>
2022-10-23 03:10:03 +0200

    START

---
+++ ft_printf/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: itopchu <itopchu@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/23 02:54:46 by itopchu       #+#    #+#                 */
+/*   Updated: 2022/10/23 02:54:46 by itopchu       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("",);
+	printf("",);
+	printf("",);
+	printf("",);
+	printf("",);
+	printf("",);
+	printf("",);
+	printf("",);
+	printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	ft_printf("",);
+	return (0);
+}
